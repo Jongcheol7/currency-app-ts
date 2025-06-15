b5c95d4ac8f89c51ef79a15381021fb044cd5c00
Lee JongCheol <whdcjf817@naver.com>
2025-06-15 10:13:29 +0900

    뒤로가기 버튼 추가

--- app/currency/exchangeMap/page.tsx
+++ app/currency/exchangeMap/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
 import ExchangeMap from "./ExchangeMap";
 export default function ExchangeMapPage() {
+  const router = useRouter();
   return (
     <div className="">
-      <p className="text-center p-2 my-2 font-bold">🗺️ 환전소 위치 🗺️</p>
+      <div className="flex justify-between">
+        <p
+          className="py-2 my-2 font-bold hover:text-blue-500"
+          onClick={() => router.push("/")}
+        >
+          뒤로가기
+        </p>
+        <p className="py-2 my-2 font-bold">🗺️ 환전소 위치 🗺️</p>
+        <p></p>
+      </div>
       <ExchangeMap />
     </div>
   );
